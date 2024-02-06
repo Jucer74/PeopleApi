ea9333d4051c43def0fcb7702ed44235a1aabf39
Julio Robles <Jucer74@Hotmail.com>
2024-02-06 01:04:40 -0500

    Update Version

--- Code/PeopleApp/People.Api/Program.cs
+++ Code/PeopleApp/People.Api/Program.cs
@@ -1,26 +1,46 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using People.Api.Extensions;
+using People.Infrastructure.Context;
 
-namespace People.Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Name=PeopleDB"));
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Modules
+builder.Services.AddCoreModules();
+builder.Services.AddInfrastructureModules();
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-   public class Program
-   {
-      public static void Main(string[] args)
-      {
-         CreateHostBuilder(args).Build().Run();
-      }
-
-      public static IHostBuilder CreateHostBuilder(string[] args) =>
-          Host.CreateDefaultBuilder(args)
-              .ConfigureWebHostDefaults(webBuilder =>
-              {
-                 webBuilder.UseStartup<Startup>();
-              });
-   }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseCors(options => {
+    options.WithOrigins("http://localhost:3000");
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+
+app.MapControllers();
+
+app.Run();
--- Code/PeopleApp/People.Domain/Exceptions/BadRequestException.cs
+++ Code/PeopleApp/People.Domain/Exceptions/BadRequestException.cs
@@ -1,28 +1,21 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace People.Domain.Exceptions
 {
-   [Serializable]
-   public class BadRequestException : Exception
-   {
-      public BadRequestException()
-      {
-      }
+    [Serializable]
+    public class BadRequestException : Exception
+    {
+        public BadRequestException()
+        {
+        }
 
-      public BadRequestException(string message) : base(message)
-      {
-      }
+        public BadRequestException(string message) : base(message)
+        {
+        }
 
-      public BadRequestException(string message, Exception innerException)
-          : base(message, innerException)
-      {
-      }
-
-      // Without this constructor, deserialization will fail
-      protected BadRequestException(SerializationInfo info, StreamingContext context)
-          : base(info, context)
-      {
-      }
-   }
+        public BadRequestException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
 }
